f758de546f85a2e0f569fe55d92af29ea213424b
NoDmAn1985 <nodman@mail.ru>
2018-04-24 20:31:28 +0700

    Beginning

---
+++ src/sample/MainController.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private RadioButton extraFromMinimumCaption;
+
+    @FXML
+    private TextField extraFromMinimumArgument;
+
+    @FXML
+    private TextField extraFromMinimumResult;
+
+    @FXML
+    private Button extraFromMinimumCopyButton;
+
+    @FXML
+    private Label minimumCaption;
+
+    @FXML
+    private RadioButton interpolationCaption;
+
+    @FXML
+    private Label maximumCaption;
+
+    @FXML
+    private RadioButton extraFromMaximumCaption;
+
+
+}
